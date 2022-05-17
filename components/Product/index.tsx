48a793a257ef5fb856cb9dedae213024c04fb5f0
umarmuhd <umarfarouqft@gmail.com>
2022-05-17 19:34:06 +0100

    added some products

---
+++ components/Product/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export const Product = (product) => {
+  const { id, image, name, price } = product;
+
+  const buyProduct = () => {};
+  const addToCart = () => {};
+  return (
+    <article className="group relative">
+      <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden lg:h-80 lg:aspect-none">
+        <img
+          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+          src={image}
+          alt=""
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <h2 className="text-sm text-gray-700">
+          <span aria-hidden="true" className="absolute inset-0" />
+          {name}
+        </h2>
+        <p className="text-sm font-medium text-gray-900">$ {price}</p>
+      </div>
+      <button
+        onClick={buyProduct}
+        className="mt-6 group outline-none relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
+      >
+        Kup
+      </button>
+      <button
+        onClick={addToCart}
+        className="mt-4 group outline-none relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-black bg-gray-100 hover:bg-gray-300 focus:outline-none"
+      >
+        Dodaj do koszyka
+      </button>
+    </article>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,43 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Navbar from '../components/Navbar';
+import { Product } from '../components/Product';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const products = [
+    {
+      id: 1,
+      image: '/images/shirt.jpg',
+      name: 'Mens Casual Premium Slim Fit T-Shirts',
+      price: 19,
+    },
+    {
+      id: 2,
+      image: '/images/9fbb62343426e1f157144f26d9b59be1629ef7c1-600x600.webp',
+      name: 'boAt Party Pal 50',
+      price: 56,
+    },
+    {
+      id: 3,
+      image: '/images/71li-ujtlUL._AC_UX679_.jpg',
+      name: 'Mens Cotton Jacket',
+      price: 55.99,
+    },
+    {
+      id: 4,
+      image: '/images/81QpkIctqPL._AC_SX679_.jpg',
+      name: 'Acer SB220Q bi 21.5 inches Full HD',
+      price: 599,
+    },
+  ];
+  return (
+    <div>
+      <Navbar />
+      <div className="bg-white max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {products &&
+          products.map((product) => <Product key={product.id} {...product} />)}
+      </div>
+    </div>
+  );
 }
