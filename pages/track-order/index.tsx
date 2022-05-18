bf35bbe135e71bebfd98ccd053ea1a6f0a8b89fe
umarmuhd <umarfarouqft@gmail.com>
2022-05-18 10:14:35 +0100

    fix deployment

--- pages/track-order/index.tsx
+++ pages/track-order/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { Footer } from '../../components/Footer';
 import Navbar from '../../components/Navbar';
@@ -49,16 +50,17 @@ export default function TrackOrder() {
           <p className="max-w-xl mb-4 text-base text-gray-700 sm:mx-auto">
             Lorem ipsum dolor sit amet, consectetur adipiscing.
           </p>
-          <a
-            href="/"
-            aria-label=""
-            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-          >
-            Learn more
-            <svg className="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
-              <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
-            </svg>
-          </a>
+          <Link href="/">
+            <a
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+              <svg className="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
+                <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+              </svg>
+            </a>{' '}
+          </Link>
         </div>
       </div>
       <Footer />
