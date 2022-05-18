4e878a3921d2c672b27904a6cc0f6564715ac1c9
umarmuhd <umarfarouqft@gmail.com>
2022-05-18 11:34:57 +0100

    update

--- pages/order-complete/index.tsx
+++ pages/order-complete/index.tsx
@@ -7,8 +7,8 @@ export default function OrderComplete() {
         <div className="w-full mx-auto md:w-3/4 bg-white px-4 md:px-8 py-4">
           <img src="/images/success-message.svg" alt="..." className="mx-auto w-2/5" />
           <div className="mt-5 text-center">
-            <h1 className="text-xl text-green-800 font-bold mb-3">Order Successfull</h1>
-            <p className="text-gray-900">Thank you for your transaction.</p>
+            <h1 className="text-xl text-green-800 font-bold mb-3">Thank You For Your Order</h1>
+            <p className="text-gray-900">Check your email inbox for the receipt.</p>
           </div>
         </div>
       </div>
